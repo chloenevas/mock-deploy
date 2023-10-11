7a0859f1b1ec2703e3c141f6efdb2b0a319edac1
RobertoGonzalesMatos <roberto_gonzales_matos@brown.edu>
2023-10-11 19:07:18 -0400

    Feat: fixed output bug

--- mock/src/components/REPL/History.tsx
+++ mock/src/components/REPL/History.tsx
@@ -10,29 +10,53 @@ interface HistoryProps {
  * @param props - HistoryProps that contains the history list
  * @returns - element of history properly formatted
  */
+var breif: boolean = true;
 export function History(props: HistoryProps) {
   return (
     <div className="history">
       {props.history.map((entry) => {
         if (typeof entry === "string") {
+          if (entry.includes("Command: ")) {
+            breif = false;
+          } else {
+            breif = true;
+          }
           return <p>{entry}</p>;
         } else {
-          return (
-            <div>
-              <p>Output:</p>
-              <table>
-                <tbody>
-                  {entry.map((row) => (
-                    <tr>
-                      {row.map((item) => (
-                        <td>{item}</td>
-                      ))}
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          );
+          if (breif) {
+            return (
+              <div>
+                <table>
+                  <tbody>
+                    {entry.map((row) => (
+                      <tr>
+                        {row.map((item) => (
+                          <td>{item}</td>
+                        ))}
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            );
+          } else {
+            return (
+              <div>
+                <p>Output:</p>
+                <table>
+                  <tbody>
+                    {entry.map((row) => (
+                      <tr>
+                        {row.map((item) => (
+                          <td>{item}</td>
+                        ))}
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            );
+          }
         }
       })}
     </div>
